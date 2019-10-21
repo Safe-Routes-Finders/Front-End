d744eb93cb3155d847e7a132929c64e90b5ed485
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-21 17:28:32 -0400

    Commented out Validation

--- safe-routes/src/Components/Forms/SignUp.jsx
+++ safe-routes/src/Components/Forms/SignUp.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from "react";
 import {Btn, FormContainer, Title, Label, Input, LinkContainer, StyledLink, ParentContainer, Form, SubTitle, ImgContainer, Error} from "./formsStyle";
-import * as Yup from "yup"
 
 function SignUp(){
     const [formValues, setFormValues] = useState({
@@ -24,35 +23,44 @@ function SignUp(){
         setFormValues({...formValues, [e.target.name]: e.target.value});
     }
 
-    const validate = () => {
-        if(!formValues.email.includes('@')){
-            setFormValues({...formValues , ["emailError"]: "Invalid Email" })
-            return false;
-        } else if(formValues.firstName.length === 0){
-            setFormValues({...formValues , ["firstNameError"]: "Enter Your First Name" })
-            return false;
-        } else if(formValues.lastName.length === 0){
-            setFormValues({...formValues , ["lastNameError"]: "Enter Your Last Name" })
-            return false;
-        } else if(formValues.username.length === 0){
-            setFormValues({...formValues , ["usernameError"]: "Enter An Username, You Will Use This To Sign In" })
-            return false;
-        } else if(formValues.password.length < 7 || formValues.password.length > 15){
-            setFormValues({...formValues , ["passwordError"]: "Password Must Be Between 8 and 14 Characters" })
-            return false;
-        } else if(formValues.password !== formValues.checkPassword){
-            setFormValues({...formValues , ["checkPasswordError"]: "Passwords Must Match" })
-            return false;
-        }
-        return true;
-    }
+
+    //  DONT DELETE STILL WORKING ON IT
+    // const validate = () => {
+    //     if(!formValues.email.includes('@') || !formValues.email.includes('.') ){
+    //         setFormValues({...formValues , ["emailError"]: "Invalid Email" })
+    //         return false;
+    //     } else if(formValues.firstName.length === 0){
+    //         setFormValues({...formValues , ["firstNameError"]: "Enter Your First Name" })
+    //         return false;
+    //     } else if(formValues.lastName.length === 0){
+    //         setFormValues({...formValues , ["lastNameError"]: "Enter Your Last Name" })
+    //         return false;
+    //     } else if(formValues.username.length === 0){
+    //         setFormValues({...formValues , ["usernameError"]: "Enter An Username, You Will Use This To Sign In" })
+    //         return false;
+    //     } else if(formValues.password.length < 7 || formValues.password.length > 15){
+    //         setFormValues({...formValues , ["passwordError"]: "Password Must Be Between 8 and 14 Characters" })
+    //         return false;
+    //     } else if(formValues.password !== formValues.checkPassword){
+    //         setFormValues({...formValues , ["checkPasswordError"]: "Passwords Must Match" })
+    //         return false;
+    //     } else{
+    //         setFormValues({...formValues,
+    //             ["firstNameError"]: "",
+    //             ["lastNameError"]: "",
+    //             ["usernameError"]: "",
+    //             ["passwordError"]: "",
+    //             ["checkPasswordError"]: "",
+    //         })
+    //         return true;
+    //     }
+    // }
     const Submit = (e) => {
         e.preventDefault();
-        const isValid = validate();
-        if(isValid){
+        // const isValid = validate();
+        // if(isValid){
             console.log("works for now", formValues)
-        }
-        console.log(formValues.emailError)
+        // }
     }
 
     return(
