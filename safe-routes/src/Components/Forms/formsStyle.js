dab3516f4bec0c6ef834f4e868670db34981dc38
Pedro Prieto <pedro.prieto003@gmail.com>
2019-10-21 21:57:51 -0400

    Only needs error messages

--- safe-routes/src/Components/Forms/formsStyle.js
+++ safe-routes/src/Components/Forms/formsStyle.js
@@ -13,16 +13,18 @@ export const FormContainer = styled.div`
 `
 
 export const Btn = styled.button `
-    background: blueviolet;
+    background: #16416C;
     color: white;
     text-transform: uppercase;
     font-weight: 700;
     padding: 2rem 7rem;
+    border: none;
     cursor: pointer;
 `
 
 export const Title = styled.h1 `
     font-size: 5rem;
+    color: #16416c
 `
 
 export const Label = styled.label `
@@ -34,7 +36,7 @@ export const Label = styled.label `
 export const Input = styled.input `
     display: block;
     border: none;
-    border-bottom: 1px blueviolet solid;
+    border-bottom: 1px #16416C solid;
     margin: 0.5rem 0;
     padding: 0.6rem 0.3rem;
     width: 100%;
@@ -59,13 +61,12 @@ export const ParentContainer = styled.div `
     width: 100%;
     display: flex;
     border-radius: 1rem;
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
     height: 100vh;
 `
 
 export const ImgContainer = styled.div `
     flex: 5;
-    background:linear-gradient(90deg, rgba(255, 255, 255,1) 0%, rgb(255, 255, 255,0.5) 100%), url("https://images.unsplash.com/photo-1524645343120-a4ae9f7d4343?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80") no-repeat center center /cover;
+    background:linear-gradient(90deg, rgba(255, 255, 255,1) 0%, rgb(91, 100, 155,0.7) 100%), url("https://images.unsplash.com/photo-1524645343120-a4ae9f7d4343?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80") no-repeat center center /cover;
     height: 100%;
     display: flex;
     justify-content: center;
@@ -74,6 +75,7 @@ export const ImgContainer = styled.div `
 
 export const SubTitle = styled.h2 `
     font-size: 3rem;
+    color: #1E2635;
 `
 
 export const Form = styled.form`
